4082d355e0a4cbe65776caf2448bddc951253280
ohyajk <ohyajk@gmail.com>
2023-04-04 13:18:58 +0530

    Fix Linters

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,13 @@
 class PostsController < ApplicationController
   def index; end
+
   def index
     @user = User.find(params[:user_id])
     @posts = @user.posts.includes(:comments, :likes)
   end
 
   def show; end
+
   def show
     @post = Post.find(params[:id])
     @user = @post.user
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,12 @@
 class UsersController < ApplicationController
   def index; end
+
   def index
     @users = User.all
   end
 
   def show; end
+
   def show
     @user = User.find(params[:id])
   end
